6060d24d6e21d93f43189592913dace04c6e27b6
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-03-09 15:49:20 +0700

    Create PBO2_2101092055.java

---
+++ PBO2_2101092055/src/pbo2_2101092055/PBO2_2101092055.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_2101092055;
+
+import abel.dao.Koneksi;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author User
+ */
+public class PBO2_2101092055 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        try {
+            Koneksi koneksi = new Koneksi ();
+            Connection con = koneksi.getKoneksi();
+            JOptionPane.showMessageDialog(null, "Koneksi ok");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error: "+ ex.getMessage());
+            Logger.getLogger(PBO2_2101092055.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
