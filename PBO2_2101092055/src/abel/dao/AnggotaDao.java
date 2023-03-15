171d8ec5bb41a25e40d15ce30df20f55a178ce96
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-03-15 12:35:56 +0700

    Create AnggotaDao.java

---
+++ PBO2_2101092055/src/abel/dao/AnggotaDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abel.dao;
+
+import abel.model.Anggota;
+import java.sql.Connection;
+
+/**
+ *
+ * @author User
+ */
+public interface AnggotaDao {
+    void insert(Connection con,Anggota anggota)throws Exception;
+    
+}
