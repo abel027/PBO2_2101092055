726d3e8ff347a28353b887bf246ca619d9b25f7b
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-05-03 12:30:27 +0700

    Update AnggotaDao.java

--- PBO2_2101092055/src/abel/dao/AnggotaDao.java
+++ PBO2_2101092055/src/abel/dao/AnggotaDao.java
@@ -6,6 +6,7 @@
 package abel.dao;
 
 import abel.model.Anggota;
+import java.util.List;
 import java.sql.Connection;
 
 /**
@@ -13,6 +14,9 @@ import java.sql.Connection;
  * @author User
  */
 public interface AnggotaDao {
-    void insert(Connection con,Anggota anggota)throws Exception;
-    
+    void insert(Connection con, Anggota anggota) throws Exception;
+    void update(Connection con, Anggota anggota) throws Exception;
+    void delete(Connection con, Anggota anggota) throws Exception;
+    public Anggota getAnggota(Connection con, String Kode) throws Exception;
+    public List<Anggota> getAllAnggota(Connection con) throws Exception;
 }
