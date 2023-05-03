e6753a81df1dcace9e1cf0e2c358b3c2a9c5e46c
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-05-03 12:31:07 +0700

    Update Anggota.java

--- PBO2_2101092055/src/abel/model/Anggota.java
+++ PBO2_2101092055/src/abel/model/Anggota.java
@@ -10,56 +10,54 @@ package abel.model;
  * @author User
  */
 public class Anggota {
-    private String kodeAnggota;
-    private String namaAnggota;
+    private String kodeanggota;
+    private String namaanggota;
     private String alamat;
-    private String jenisKelamin;
+    private String jeniskelamin;
     
     public Anggota(){
-      
+        
     }
 
-    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
-        this.kodeAnggota = kodeAnggota;
-        this.namaAnggota = namaAnggota;
+    public Anggota(String kodeanggota, String namaanggota, String alamat, String jeniskelamin) {
+        this.kodeanggota = kodeanggota;
+        this.namaanggota = namaanggota;
         this.alamat = alamat;
-        this.jenisKelamin = jenisKelamin;
+        this.jeniskelamin = jeniskelamin;
     }
     
     
-
-    public String getKodeAnggota() {
-        return kodeAnggota;
+    public String getKodeanggota() {
+        return kodeanggota;
     }
 
-    public String getNamaAnggota() {
-        return namaAnggota;
+    public void setKodeanggota(String kodeanggota) {
+        this.kodeanggota = kodeanggota;
+    }
+
+    public String getNamaanggota() {
+        return namaanggota;
+    }
+
+    public void setNamaanggota(String namaanggota) {
+        this.namaanggota = namaanggota;
     }
 
     public String getAlamat() {
         return alamat;
     }
 
-    public String getJenisKelamin() {
-        return jenisKelamin;
-    }
-
-    public void setKodeAnggota(String kodeAnggota) {
-        this.kodeAnggota = kodeAnggota;
-    }
-
-    public void setNamaAnggota(String namaAnggota) {
-        this.namaAnggota = namaAnggota;
-    }
-
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
 
-    public void setJenisKelamin(String jenisKelamin) {
-        this.jenisKelamin = jenisKelamin;
+    public String getJeniskelamin() {
+        return jeniskelamin;
+    }
+
+    public void setJeniskelamin(String jeniskelamin) {
+        this.jeniskelamin = jeniskelamin;
     }
     
     
-
 }
