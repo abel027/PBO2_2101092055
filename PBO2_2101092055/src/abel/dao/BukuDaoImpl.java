62db8a961a976951a8ff3b379b3fb19ca56c53f4
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-07-05 12:55:42 +0700

    Update BukuDaoImpl.java

--- PBO2_2101092055/src/abel/dao/BukuDaoImpl.java
+++ PBO2_2101092055/src/abel/dao/BukuDaoImpl.java
@@ -5,41 +5,28 @@
  */
 package abel.dao;
 
-import abel.model.Buku;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import abel.model.Buku;
 
 /**
  *
  * @author User
  */
- public class BukuDaoImpl implements BukuDao {
+public class BukuDaoImpl implements BukuDao {
 
     @Override
     public void insert(Connection con, Buku buku) throws Exception {
-        String sql = "insert into buku values(?,?,?,?)";
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, buku.getKodebuku());
-        ps.setString(2, buku.getJudul());
-        ps.setString(3, buku.getPengarang());
-        ps.setString(4, buku.getPenerbit());
-        ps.executeUpdate();
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
     @Override
     public void update(Connection con, Buku buku) throws Exception {
-            String sql = 
-                "update buku set judul=?, pengarang=?,"
-                + "penerbit=? where kodebuku=?";
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, buku.getKodebuku());
-        ps.setString(2, buku.getJudul());
-        ps.setString(3, buku.getPengarang());
-        ps.setString(4, buku.getPenerbit());
-        ps.executeUpdate();
-    }        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
     @Override
     public void delete(Connection con, Buku buku) throws Exception {
@@ -47,63 +34,38 @@ import java.util.List;
     }
 
     @Override
-    public Buku getAnggota(Connection con, String Kode) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Buku getBuku(Connection con, String kode) throws Exception {
+        String sql = "select * from buku where kodebuku = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, kode);
+        ResultSet rs = ps.executeQuery();
+        Buku buku = null;
+        if (rs.next()) {
+            buku = new Buku();
+            buku.setKodebuku(rs.getString(1));
+            buku.setJudulbuku(rs.getString(2));
+            buku.setPengarang(rs.getString(3));
+            buku.setPenerbit(rs.getString(4));
+        }
+        return buku;
     }
 
     @Override
     public List<Buku> getAllBuku(Connection con) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    }
-    
-    @Override
-    public void delete(Connection con, Buku buku) throws Exception {
-        String sql = 
-                "delete from buku"
-                +"where kodebuku = ?";
+        String sql = "select * from buku";
         PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, buku.getKodebuku());
-        ps.setString(2, buku.getJudul());
-        ps.setString(3, buku.getPengarang());
-        ps.setString(4, buku.getPenerbit());
-        ps.executeUpdate();
-    }
-    
-    @Override
-    public Buku getBuku(Connection con, String kode) throws Exception {
-        String sql = "select * from buku "
-                + "where kodebuku = ?" ; 
-        PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, kode);
         ResultSet rs = ps.executeQuery();
-        Anggota anggota = null;
-        if (rs.next()) {
-            buku = new Anggota();
+        List<Buku> list = new ArrayList<>();
+        Buku buku = null;
+        while (rs.next()) {
+            buku = new Buku();
             buku.setKodebuku(rs.getString(1));
-            buku.setjudulrs.getString(2));
+            buku.setJudulbuku(rs.getString(2));
             buku.setPengarang(rs.getString(3));
             buku.setPenerbit(rs.getString(4));
-        }
-        return anggota; 
-    }
-    
-    @Override
-    public List<Anggota> getAllAnggota(Connection con) throws Exception {
-        String sql = "select * from anggota";
-        PreparedStatement ps = con.prepareCall(sql);
-        ResultSet rs = ps.executeQuery();
-        List<Anggota> list = new ArrayList<>();
-        Anggota anggota = null;
-        while (rs.next()){
-            anggota = new Anggota();
-            anggota.setKodeanggota(rs.getString(1));
-            anggota.setNamaanggota(rs.getString(2));
-            anggota.setAlamat(rs.getString(3));
-            anggota.setJeniskelamin(rs.getString(4));
-            list.add(anggota);
+            list.add(buku);
         }
         return list;
     }
-}
+    
 }
