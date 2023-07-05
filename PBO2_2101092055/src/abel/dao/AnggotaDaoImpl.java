8628812ca51d1f995690415511fa2b162280e245
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-07-05 13:07:45 +0700

    Update AnggotaDaoImpl.java

--- PBO2_2101092055/src/abel/dao/AnggotaDaoImpl.java
+++ PBO2_2101092055/src/abel/dao/AnggotaDaoImpl.java
@@ -5,14 +5,12 @@
  */
 package abel.dao;
 
-import abel.model.Anggota;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import static java.util.Collections.list;
 import java.util.List;
-
+import abel.model.Anggota;
 
 /**
  *
@@ -31,37 +29,30 @@ public class AnggotaDaoImpl implements AnggotaDao {
         ps.executeUpdate();
     }
 
-    
     @Override
     public void update(Connection con, Anggota anggota) throws Exception {
-            String sql = 
-                "update anggota set namaanggota=?, alamat=?,"
-                + "jeniskelamin=? where kodeanggota=?";
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, anggota.getKodeanggota());
-            ps.setString(2, anggota.getNamaanggota());
-            ps.setString(3, anggota.getAlamat());
-            ps.setString(4, anggota.getJeniskelamin());
-            ps.executeUpdate();         
-    }
-    
-    @Override
-    public void delete(Connection con, Anggota anggota) throws Exception {
-        String sql = 
-                "delete from anggota"
-                +"where kodeanggota = ?";
+        String sql = "update anggota set namaanggota=?, alamat=?, jekel=? "
+                + "where kodeanggota=?";
         PreparedStatement ps = con.prepareStatement(sql);
-        ps.setString(1, anggota.getKodeanggota());
-        ps.setString(2, anggota.getNamaanggota());
-        ps.setString(3, anggota.getAlamat());
-        ps.setString(4, anggota.getJeniskelamin());
+        ps.setString(1, anggota.getNamaanggota());
+        ps.setString(2, anggota.getAlamat());
+        ps.setString(3, anggota.getJeniskelamin());
+        ps.setString(4, anggota.getKodeanggota());
         ps.executeUpdate();
     }
-    
+
+    @Override
+    public void delete(Connection con, Anggota anggota) throws Exception {
+        String sql = "delete from anggota "
+                + "where kodeanggota = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, anggota.getKodeanggota());
+        ps.executeUpdate();
+    }
+
     @Override
     public Anggota getAnggota(Connection con, String kode) throws Exception {
-        String sql = "select * from anggota "
-                + "where kodeanggota = ?" ; 
+        String sql = "select * from anggota where kodeanggota = ?";
         PreparedStatement ps = con.prepareStatement(sql);
         ps.setString(1, kode);
         ResultSet rs = ps.executeQuery();
@@ -73,17 +64,17 @@ public class AnggotaDaoImpl implements AnggotaDao {
             anggota.setAlamat(rs.getString(3));
             anggota.setJeniskelamin(rs.getString(4));
         }
-        return anggota; 
+        return anggota;
     }
-    
+
     @Override
     public List<Anggota> getAllAnggota(Connection con) throws Exception {
         String sql = "select * from anggota";
-        PreparedStatement ps = con.prepareCall(sql);
+        PreparedStatement ps = con.prepareStatement(sql);
         ResultSet rs = ps.executeQuery();
         List<Anggota> list = new ArrayList<>();
         Anggota anggota = null;
-        while (rs.next()){
+        while (rs.next()) {
             anggota = new Anggota();
             anggota.setKodeanggota(rs.getString(1));
             anggota.setNamaanggota(rs.getString(2));
@@ -93,4 +84,5 @@ public class AnggotaDaoImpl implements AnggotaDao {
         }
         return list;
     }
+
 }
