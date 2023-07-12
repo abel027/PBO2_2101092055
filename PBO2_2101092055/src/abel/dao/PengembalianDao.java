202664b671195aa5e2cc370a82c7606f318eae4f
Muhammad Abbel Prasetya <114122498+abel027@users.noreply.github.com>
2023-07-12 09:44:44 +0700

    Update PengembalianDao.java

--- PBO2_2101092055/src/abel/dao/PengembalianDao.java
+++ PBO2_2101092055/src/abel/dao/PengembalianDao.java
@@ -17,7 +17,8 @@ public interface PengembalianDao {
     public void insert(Connection con, Pengembalian pengembalian) throws Exception;
     public void update(Connection con, Pengembalian pengembalian) throws Exception;
     public void delete(Connection con, Pengembalian pengembalian) throws Exception;
-    public Pengembalian getPengembalian(Connection con, 
-            String kodeanggota, String kodebuku, String tglpinjam) throws Exception;
+    public Pengembalian getPengembalian(Connection con, String kodeanggota, String kodebuku, String tglpinjam) throws Exception;
     public List<Pengembalian> getAllPengembalian(Connection con) throws Exception;
+    public int selisihTanggal (Connection con, String tgl1, String tgl2)  throws Exception;
+
 }
